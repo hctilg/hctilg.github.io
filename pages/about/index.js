3b276c64b6961671d0ea12868bd0c437aeaa5785
Hctilg <MsfNemati1386@gmail.com>
2024-07-05 06:54:58 +0330

    Updated

--- pages/about/index.js
+++ pages/about/index.js
@@ -10,18 +10,37 @@ export default function About({ Component, pageProps }) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Glitch: About</title>
     </Head>
-    <div>
-      <span>This page will be ready soon...</span>
+    <div className='content'>
+      <div>
+        <h2>Hi, I'm <b>Mahi</b></h2>
+        <p>A programmer, writer, etc ...</p>
+        <p>I enjoy nature, science, and music :3</p>
+        <p>This website is just for passing time and archiving some of my knowledge and stories.</p>
+        <p>My field of work is not limited, but I'm more interested in AI, web and software development in programming.</p>
+        <p>I love traveling alone, uninterrupted reading of books, swimming, and dancing.</p>
+        <p>I like Jasmine and Viola(violet) flowers.</p>
+        <p>I don't care about anything other than my goals, if you want to be noticed, put yourself on the path of my goals :)</p>
+        <p>I like piano and violin ...</p>
+      </div>
     </div>
     <style jsx>{`
+.content {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  min-height: calc((60 * (var(--height) - 50px)) / 100);
+}
+
 h2 {
-  margin: 30px 0 20px;
+  margin: 20px 10px 15px;
+  font-size: 22px;
 }
-span {
-  font-size: 20px;
-}
-img {
-  margin: 30px auto 0;
+
+p {
+  line-height: 1.7;
+  margin: 5px 14px;
+  font-size: 18px;
 }
   `}</style>
   </>
